a7c7431e6218fa1bd91198abe19480437a574e7e
Tobias Heatlie <Tobias.Heatlie@sbs.co.uk>
2022-01-04 11:19:39 +0000

    Add project files.

---
+++ Armour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    internal class Armour : Component
+    {
+        public static int FORWARD = 0;
+        public static int LEFT = 1;
+        public static int BACK = 2;
+        public static int RIGHT = 3;
+        public readonly int Side;
+
+        public Armour(int health, int Side)
+        {
+            TotalHealth = health;
+            this.Side = Side;
+        }
+    }
+}
---
+++ Component.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    internal class Component
+    {
+        public int TotalHealth { get; protected set; }
+        public int DamagedHealth { get; protected set; }
+
+        public bool Destroyed
+        { get { return TotalHealth == DamagedHealth; } }
+
+        public void Damage(int Damage)
+        {
+            if (!Destroyed)
+            {
+                DamagedHealth += Damage;
+                if (DamagedHealth >= TotalHealth)
+                    DamagedHealth = TotalHealth;
+            }
+        }
+
+        public void Restore()
+        {
+            DamagedHealth = 0;
+        }
+    }
+}
