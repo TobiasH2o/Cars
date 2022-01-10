96c59f61042ae518d27f63f111239da3870db05c
Tobias Forbes Heatile <56791987+TobiasH2o@users.noreply.github.com>
2022-01-10 21:27:00 +0000

    Added map generation

---
+++ MapComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class MapComponent : VisualComponent
+    {
+
+        public int Xpos { get; private set; }
+        public int Ypos { get; private set; }
+        private int Traversable;
+        public MapComponent(int Xpos, int Ypos, int Width, int Height, int? Traversable = null) : base(Width, Height, "")
+        {
+            this.Xpos = Xpos;
+            this.Ypos = Ypos;
+            this.Traversable = Traversable ?? -1;
+        }
+
+        public bool IsTraversable(int VehicleType)
+        {
+            return VehicleType <= Traversable;
+        }
+    }
+}
---
+++ VehicleTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class VehicleTypes
+    {
+        public static readonly int CAR = 2;
+        public static readonly int BIKE = 1;
+        public static readonly int PEDESTRIAN = 0;
+    }
+}
